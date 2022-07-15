22aea0f4dd83d4b34d2a8ad73b9189dc6a8d2965
PauloAlves8039 <pj38alves@gmail.com>
2022-07-14 22:03:57 -0300

    Criando e implementando a interface IRepository

---
+++ src/NerdStore.Core/DomainObjects/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NerdStore.Core.DomainObjects.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
