db323be9780f4268273c5937a528be84e01a4510
PauloAlves8039 <pj38alves@gmail.com>
2022-07-17 21:01:47 -0300

    Criando eventos de domínio

--- src/NerdStore.Catalogo.Domain/Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/Domain/EstoqueService.cs
@@ -1,4 +1,6 @@
-﻿using NerdStore.Catalogo.Domain.Interfaces;
+﻿using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Catalogo.Domain.Interfaces;
+using NerdStore.Core.Bus;
 using System;
 using System.Threading.Tasks;
 
@@ -7,10 +9,12 @@ namespace NerdStore.Catalogo.Domain.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatrHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository)
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
@@ -22,6 +26,11 @@ namespace NerdStore.Catalogo.Domain.Domain
             if (!produto.PossuiEstoque(quantidade)) return false;
 
             produto.DebitarEstoque(quantidade);
+
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             _produtoRepository.Atualizar(produto);
 
---
+++ src/NerdStore.Core/Messages/CommonMessages/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NerdStore.Core.Messages.CommonMessages
+{
+    public abstract class DomainEvent : Event
+    {
+        public DomainEvent(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
