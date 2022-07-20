6c5e864b6567d2dc75dabe1afabf674f25b17f8a
PauloAlves8039 <pj38alves@gmail.com>
2022-07-19 21:11:49 -0300

    Criando classe para uso de injeção de dependência

---
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Catalogo.Data.Context;
+using NerdStore.Catalogo.Data.Repositories;
+using NerdStore.Catalogo.Domain.Domain;
+using NerdStore.Catalogo.Domain.Interfaces;
+using NerdStore.Core.Bus;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+        }
+    }
+}
