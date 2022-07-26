71373e93fbb5b18a25b80854fa1677a5de32471d
PauloAlves8039 <pj38alves@gmail.com>
2022-07-25 21:54:52 -0300

    Adicionando o método EnviarComando

--- src/NerdStore.Core/Bus/MediatrHandler.cs
+++ src/NerdStore.Core/Bus/MediatrHandler.cs
@@ -8,7 +8,15 @@ namespace NerdStore.Core.Bus
     {
         private readonly IMediator _mediator;
 
-        
+        public MediatrHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<bool> EnviarComando<T>(T comando) where T : Command
+        {
+            return await _mediator.Send(comando);
+        }
 
         public async Task PublicarEvento<T>(T evento) where T : Event
         {
