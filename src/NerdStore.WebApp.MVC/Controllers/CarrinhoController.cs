f52c8527c5c770f525fe55207a1a06eb6f9df6ba
PauloAlves8039 <pj38alves@gmail.com>
2022-07-25 22:03:30 -0300

    Adicionando os controladores CarrinhoController e ControllerBase

---
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class CarrinhoController : ControllerBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        private readonly IMediatrHandler _mediatrHandler;
+
+        public CarrinhoController(IProdutoAppService produtoAppService, IMediatrHandler mediatrHandler)
+        {
+            _produtoAppService = produtoAppService;
+            _mediatrHandler = mediatrHandler;
+        }
+
+        [HttpPost]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> AdicionarItem(Guid id, int quantidade)
+        {
+            var produto = await _produtoAppService.ObterPorId(id);
+            if (produto == null) return BadRequest();
+
+            if (produto.QuantidadeEstoque < quantidade)
+            {
+                TempData["Erro"] = "Produto com estoque insuficiente";
+                return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+            }
+
+            var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
+
+
+            TempData["Erros"] = "Produto Indisponível";
+            return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+        }
+    }
+}
