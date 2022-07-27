494d0394a84e525bf2e51513598ff052b0e883bd
PauloAlves8039 <pj38alves@gmail.com>
2022-07-26 21:32:23 -0300

    Configurando contexto do banco de dados para Vendas

---
+++ src/NerdStore.Vendas.Data/Mappings/PedidoMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Vendas.Domain.Domain;
+
+namespace NerdStore.Vendas.Data.Mappings
+{
+    public class PedidoMapping : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Codigo)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.HasMany(c => c.Pedidos)
+                .WithOne(c => c.Voucher)
+                .HasForeignKey(c => c.VoucherId);
+
+            builder.ToTable("Vouchers");
+        }
+    }
+}
